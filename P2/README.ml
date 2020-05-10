b3c42a2249298bc2014596ca39ec74e19690b0d1
Kin Cheung <email_kin@yahoo.com>
2020-05-10 12:35:05 +0800

    Add files via upload

---
+++ P2/README.ml
@@ -0,0 +1,8 @@
+# Machine Learning Engineer Nanodegree Program
+
+## Dog breed classifier using CNN
+
+This part of the repository contains the following:
+ - A proposal to the dog breed classifier project
+ - A python workbook that contains code that produces the required results. It concludes with a demotration of the final application of a dog breed classifier
+ 
